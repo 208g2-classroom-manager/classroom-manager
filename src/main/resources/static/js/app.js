dea1bacbaab72c278fe21aa48e526896a24ecd6a
tupm58 <kittkatt.liucuxiu@gmail.com>
2016-06-09 20:16:03 +0700

    first commit

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,16 @@
+clvar app = angular.module('classRoomManager', []);
+
+app.controller("ClassRoomCtrl", function ($scope, $http) {
+
+    $scope.classRooms = [];
+
+    $scope.loadClassRooms = function () {
+        var httpRequest = $http({
+            method: 'GET',
+            url: 'http://localhost:8080/classes'
+
+        }).success(function (data, status) {
+            $scope.classRooms = data;
+        });
+    };
+});
